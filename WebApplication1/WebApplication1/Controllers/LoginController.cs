fa42f935591c81bd9ed3e3d9722154b95cd81309
Roman Tsaryk <agariogame2000@gmail.com>
2020-05-14 23:58:43 +0300

    Add project

---
+++ WebApplication1/WebApplication1/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class LoginController : ApiController
+    {
+        private readonly IICUTechservice techservice;
+
+        public LoginController()
+        {
+            techservice = new IICUTechservice();
+        }
+        
+        [HttpPost]
+        public string Login([FromBody]JObject data)
+        {
+            string username = data.GetValue("username").ToString();
+            string password = data.GetValue("password").ToString();
+            string ips = data.GetValue("ips").ToString();
+
+            var result = techservice.Login(username, password, ips);
+            
+
+            return (string)result;
+        }
+    }
+}
